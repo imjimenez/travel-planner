d3cb2edb22fff77b3c90f96b677f85aa898879a0
Dorkaitz Garcia <dogarcia@birt.eus>
2025-11-23 18:39:57 +0100

    install primeNG

--- src/app/features/dashboard/pages/overview/overview.component.ts
+++ src/app/features/dashboard/pages/overview/overview.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
+import { CheckButton } from "../../../../shared/components/check-button/check-button";
 
 @Component({
 	selector: "app-overview",
-	imports: [],
+	imports: [CheckButton],
 	template: `
     <div>
       <h1>Welcome to the Overview</h1>
       <p>This is the overview component.</p>
+      <app-check-button></app-check-button>
     </div>
   `,
 	styles: [],
